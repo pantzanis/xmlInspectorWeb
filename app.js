b42bb4ffc145f33e802ca171057f77e258a8f72f
pantzanis <ptzanis@yahoo.com>
2021-12-18 22:45:10 +0100

    function openNewWindow defined

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ function deleteElementById(id) {
   }
 }
 
+function openNewWindow() {
+  var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+  myWindow.document.write(
+    "<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>"
+  );
+}
+
 function dropHandler(ev) {
   console.log("File(s) dropped");
 
